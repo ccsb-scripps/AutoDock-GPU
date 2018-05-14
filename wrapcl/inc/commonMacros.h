916b8c58b2b6d90dfccd9d8ad0925c632fa6d063
lvs <solis@esa.informatik.tu-darmstadt.de>
2018-05-14 13:03:25 +0200

    added #define to remove warn in compilation for cmdqueue

--- wrapcl/inc/commonMacros.h
+++ wrapcl/inc/commonMacros.h
@@ -27,6 +27,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 	//#include <malloc.h>
 
+#define CL_USE_DEPRECATED_OPENCL_1_2_APIS
 #include <stdio.h>
 #include <stdlib.h>
 #include <CL/opencl.h>
