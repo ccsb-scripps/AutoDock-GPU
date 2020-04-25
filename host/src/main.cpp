db8f09ae303dd15ea73b74b860eb73a566c764a2
Aaron Scheinberg <ascheinb@pppl.gov>
2020-04-25 00:01:31 -0400

    Refactor pipeline to allow multiple setup threads

--- host/src/main.cpp
+++ host/src/main.cpp
@@ -42,160 +42,189 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "profile.hpp"
 
 #ifndef _WIN32
-// ------------------------
 // Time measurement
 #include <sys/time.h>
-// ------------------------
+#endif
 
-inline double seconds_since(timeval& time_start)
+template<typename T>
+inline double seconds_since(T& time_start)
 {
+#ifndef _WIN32
 	timeval time_end;
 	gettimeofday(&time_end,NULL);
         double num_sec     = time_end.tv_sec  - time_start.tv_sec;
         double num_usec    = time_end.tv_usec - time_start.tv_usec;
         return (num_sec + (num_usec/1000000));
-}
+#else
+	return 0.0;
 #endif
+}
+
+template<typename T>
+inline void start_timer(T& time_start)
+{
+#ifndef _WIN32
+	gettimeofday(&time_start,NULL);
+#endif
+}
 
 int main(int argc, char* argv[])
 {
-	FileList filelist;
-	int n_files = 1; // default
-	bool overlap = false; // default
-	int setup_thread = 0; // thread the performs the setup
-	int execution_thread = 0; // thread that performs the execution
-	int err = 0;
-#ifndef _WIN32
-	// Start full timer
-	timeval time_start, loop_time_start;
-	gettimeofday(&time_start,NULL);
-	double exec_time, setup_time;
-	double total_savings=0;
-	double total_could_save=0;
+	// CPU thread setup
+	int nthreads=1;
+#ifdef USE_PIPELINE
+	#pragma omp parallel
+	{nthreads = omp_get_num_threads();}
 #endif
+	int execution_thread=nthreads-1; // Last thread does the execution
+        int nqueues=std::max(nthreads-1,1); // The other threads each get a queue
 
-	// Objects that are arguments of docking_with_gpu
-	// These must each have 2
-	Dockpars   mypars[2];
-	Liganddata myligand_init[2];
-	Gridinfo   mygrid[2];
-	Liganddata myxrayligand[2];
-	std::vector<float> floatgrids[2];
+	// Timer initializations
+#ifndef _WIN32
+	timeval time_start, idle_timer, setup_timer, exec_timer;
+	start_timer(time_start);
+	start_timer(idle_timer);
+#endif
+	double total_setup_time=0;
+	double total_exec_time=0;
+	double idle_time;
 
-	// Read all the file names if -filelist option is on
+	// File list setup if -filelist option is on
+	FileList filelist;
+	int n_files;
 	if (get_filelist(&argc, argv, filelist) != 0)
 		      return 1;
-
-	// Setup using filelist
 	if (filelist.used){
 		n_files = filelist.nfiles;
 		printf("\nRunning %d jobs in pipeline mode ", n_files);
-#ifdef USE_PIPELINE
-		overlap=true; // Not set up to work with nested OpenMP (yet)
-		execution_thread = 1; // Assign Thread 1 to do the execution
-#endif
+		printf("\nUsing %d threads", nthreads);
+	} else {
+		n_files = 1;
 	}
 
+	// Objects that are arguments of docking_with_gpu
+        Dockpars   mypars[nqueues];
+        Liganddata myligand_init[nqueues];
+        Gridinfo   mygrid[nqueues];
+        Liganddata myxrayligand[nqueues];
+        std::vector<float> floatgrids[nqueues];
+
 	// Set up run profiles for timing
+	bool get_profiles = false; // hard-coded switch to use ALS's job profiler
 	std::vector<Profile> profiles;
+	for (int i=0;i<n_files;i++){
+		profiles.push_back(Profile(i));
+		if (!get_profiles) break; // still create 1 if off
+	}
 
 	// Print version info
 	printf("\nAutoDock-GPU version: %s\n", VERSION);
 
-	for (int i_file=0;i_file<(n_files+1);i_file++){ // one extra iteration since its a pipeline
-		int s_id = i_file % 2;    // Alternate which set is undergoing setup (s_id)
-		int r_id = (i_file+1) %2; // and which is being used in the run (r_id)
-		if (filelist.used) {
-			printf("\n\n-------------------------------------------------------------------");
-			if (i_file<n_files){
-				printf("\n(Prepping Job #%d: )", i_file);
-				printf("\n(   Reading fields: %s )",  filelist.fld_files[i_file].c_str());
-				printf("\n(   Reading ligands: %s )", filelist.ligand_files[i_file].c_str()); fflush(stdout);
-			}
-			if (i_file>0){
-				if (i_file<n_files) printf("\nMeanwhile, running ");
-				if (i_file==n_files) printf("\nRunning ");
-				printf("Job #%d: ", i_file-1);
-				printf("\n   Fields from: %s",  filelist.fld_files[i_file-1].c_str());
-				printf("\n   Ligands from: %s", filelist.ligand_files[i_file-1].c_str()); fflush(stdout);
+	bool finished_all=false;
+	std::vector<int> job_in_queue(nqueues,-1);
+	std::vector<bool> ready_to_launch(nqueues,false);
+	int n_finished_jobs=0;
+	int next_job_to_setup=0;
+	int err = 0;
+#ifdef USE_PIPELINE
+	#pragma omp parallel
+	{
+	int t_id = omp_get_thread_num();
+#else
+	{
+	int t_id = 0;
+#endif
+	while (!finished_all && err==0){
+		if(t_id!=execution_thread || nthreads==1) { // This thread handles setup
+			if (!ready_to_launch[t_id] && next_job_to_setup<n_files){ // If not ready for execution
+				int i_job;
+				// Grab the next job in atomic capture so two threads don't set up the same job
+#ifdef USE_PIPELINE
+				#pragma omp atomic capture
+#endif
+				{ i_job=next_job_to_setup; next_job_to_setup++; }
+
+				// Setup the next file in the queue
+				if (i_job<n_files) {
+					printf ("\n(Thread %d is setting up job %d)",t_id,i_job); fflush(stdout);
+					job_in_queue[t_id]=i_job;
+					start_timer(setup_timer);
+					// Load files, read inputs, prepare arrays for docking stage
+					if (setup(mygrid[t_id], floatgrids[t_id], mypars[t_id], myligand_init[t_id], myxrayligand[t_id], filelist, i_job, argc, argv) != 0) {
+						printf("\n\nError in setup of Job #%d:", i_job);
+                		                printf("\n(   Field file: %s )",  filelist.fld_files[i_job].c_str());
+		                                printf("\n(   Ligand file: %s )", filelist.ligand_files[i_job].c_str()); fflush(stdout);
+						err = 1;
+					}
+#ifdef USE_PIPELINE
+					#pragma omp atomic update
+#endif
+					total_setup_time+=seconds_since(setup_timer);
+					ready_to_launch[t_id]=true; // Indicate this queue is ready for use
+				}
+			} else { // Job is ready to go, just waiting for the GPU to free up
+				// Wait
 			}
 		}
-#ifndef _WIN32
-		// Time measurement: start of loop
-		gettimeofday(&loop_time_start,NULL);
-#endif
-		// Branch into two threads
-		//   setup_thread reads files and prepares the inputs to docking_with_gpu
-		//   execution_thread runs docking_with_gpu
-#ifdef USE_PIPELINE
-		#pragma omp parallel
-		{
-			int thread_id = omp_get_thread_num();
-#else
-		{
-			int thread_id = 0;
-#endif
-			// Thread 0 does the setup, unless its the last run (so nothing left to load)
-			if ((thread_id==setup_thread) && i_file<n_files) {
-				// Load files, read inputs, prepare arrays for docking stage
-				if (setup(mygrid[s_id], floatgrids[s_id], mypars[s_id], myligand_init[s_id], myxrayligand[s_id], filelist, i_file, argc, argv) != 0)
-					{printf("\n\nError in setup, stopped job."); err = 1;}
-			}
 
-			// Do the execution on thread 1, except on the first iteration since nothing is loaded yet
-			if ((thread_id==execution_thread) && i_file>0) {
-				Profile newprofile(i_file-1);
-				profiles.push_back(newprofile);
+		if(t_id==execution_thread) { // This thread handles the GPU
+			// Check if there is a job ready to launch
+			int i_queue=-1;
+			int i_job = -1;
+			for (int i=0;i<ready_to_launch.size();i++){
+				if (ready_to_launch[i]){
+					i_queue=i;
+					i_job=job_in_queue[i_queue];
+					break;
+				}
+			}
+			if (i_queue>=0){ 
+				idle_time = seconds_since(idle_timer);
+				start_timer(exec_timer);
+				printf("\nRunning Job #%d: ", i_job);
+                                printf("\n   Fields from: %s",  filelist.fld_files[i_job].c_str());
+                                printf("\n   Ligands from: %s", filelist.ligand_files[i_job].c_str()); fflush(stdout);
 				// Starting Docking
-				if (docking_with_gpu(&(mygrid[r_id]), floatgrids[r_id].data(), &(mypars[r_id]), &(myligand_init[r_id]), &(myxrayligand[r_id]), profiles[i_file-1], &argc, argv ) != 0)
+				if (docking_with_gpu(&(mygrid[i_queue]), floatgrids[i_queue].data(), &(mypars[i_queue]), &(myligand_init[i_queue]), &(myxrayligand[i_queue]), profiles[(get_profiles ? i_job : 0)], &argc, argv ) != 0)
 					{printf("\n\nError in docking_with_gpu, stopped job."); err = 1;}
 
-			}
-#ifndef _WIN32
-			if (thread_id==setup_thread && overlap) setup_time = seconds_since(loop_time_start);
-			if (thread_id==execution_thread && overlap) exec_time = seconds_since(loop_time_start);
-#endif
-		} // End of openmp parallel region, implicit thread barrier
-		if (err==1) return 1; // Couldnt return immediately while in parallel region
+
+				ready_to_launch[i_queue]=false; // Indicate this queue is ready for a new setup
+                                n_finished_jobs++;
+				if (n_finished_jobs==n_files) finished_all=true;
 
 #ifndef _WIN32
-		// Time measurement of this loop
-		double loop_time = seconds_since(loop_time_start);
-		printf("\nLoop run time %.3f sec \n", loop_time);
-		// Determine overlap savings (no overlap at beginning and end of pipeline)
-		if (overlap && i_file>0 && i_file<n_files){
-			double savings = overlap ? (setup_time + exec_time - loop_time) : 0;
-			total_savings += savings;
-			printf("Savings from overlap: %.3f sec \n", savings);
-			double could_save = setup_time-exec_time;
-			if (could_save>0) {
-				printf("WARNING: Setup time exceeded exec time by %.3f sec - consider adding a second a pipeline\n", could_save);
-				total_could_save += could_save;
-			}
-			printf("Cumulative savings: %.3f sec; Cumulative potential additional savings: %.3f sec", total_savings, total_could_save);
-		}
+				double exec_time = seconds_since(exec_timer);
+				total_exec_time+=exec_time;
+				printf("\nJob took %.3f sec after waiting %.3f sec for setup", exec_time, idle_time);
+				start_timer(idle_timer);
+	                        // Append time information to .dlg file
+	                        char report_file_name[256];
+	                        strcpy(report_file_name, mypars[i_queue].resname);
+	                        strcat(report_file_name, ".dlg");
+	                        FILE* fp = fopen(report_file_name, "a");
+	                        fprintf(fp, "\n\n");
+				fprintf(fp, "\nRun time %.3f sec", exec_time);
+				fprintf(fp, "\nIdle time %.3f sec\n", idle_time);
+	                        fclose(fp);
 
-		if (i_file>0){
-			// Append time information to .dlg file
-			char report_file_name[256];
-			strcpy(report_file_name, mypars[r_id].resname);
-			strcat(report_file_name, ".dlg");
-			FILE* fp = fopen(report_file_name, "a");
-			fprintf(fp, "\n\n\nRun time %.3f sec\n", loop_time);
-			fclose(fp);
-
-			// Detailed timing information to .timing
-			profiles[i_file-1].exec_time = exec_time;
-			profiles[i_file-1].write_to_file(filelist.filename);
-		}
+				if (get_profiles){
+	                        	// Detailed timing information to .timing
+	                        	profiles[i_job].exec_time = exec_time;
+	                        	profiles[i_job].write_to_file(filelist.filename);
+				}
 #endif
-		if (err==1) return 1;
-	} // end of i_file loop
+			} else { // No job is ready to launch
+				// Wait
+			}
+		} 
+	} // end of while loop
+	} // end of parallel section
 
 #ifndef _WIN32
 	// Total time measurement
 	printf("\nRun time of entire job set (%d files): %.3f sec", n_files, seconds_since(time_start));
-	if (overlap) printf("\nTotal savings from overlap: %.3f sec \n\n", total_savings); 
+	printf("\nSavings from pipelining: %.3f sec",(total_setup_time+total_exec_time) - seconds_since(time_start));
 #endif
 
 	return 0;
