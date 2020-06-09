cd7ef65600dfa3dfa0f07b92d08521ae71de4c19
atillack <atillack@scripps.edu>
2020-06-09 08:16:24 -0700

    Minor compile fix (OpenCL needed cstdint and c++11 for uint32_t).

--- host/inc/miscellaneous.h
+++ host/inc/miscellaneous.h
@@ -34,6 +34,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <stdlib.h>
 #include <ctype.h>
 #include <limits>
+#include <cstdint>
 
 #define PI 3.14159265359
 
