cb4cbcd897c0a82f81d56d4c86aa066269aae249
Pierre DARME <pierre.darme@univ-reims.fr>
2020-10-12 19:41:37 +0200

    Removed double line

--- host/src/getparameters.cpp
+++ host/src/getparameters.cpp
@@ -42,7 +42,6 @@ static inline void ltrim(std::string &s) {
 static inline void rtrim(std::string &s) {
     s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
         return !std::isspace(ch);
-        return !std::isspace(ch);
     }).base(), s.end());
 }
 
