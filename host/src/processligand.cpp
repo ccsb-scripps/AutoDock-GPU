fbd9e262ec4ca0dc1fb9aae579a3d50fe51bf6d9
atillack <atillack@scripps.edu>
2021-04-12 15:12:26 -0400

    Only perform vdW analysis for atoms that are not H-bond capable.

--- host/src/processligand.cpp
+++ host/src/processligand.cpp
@@ -1784,9 +1784,12 @@ char* analyze_ligand_receptor(
 					result += line;
 				}
 			} else{ // vdW
-				if(dist2 <= 4.2*4.2){
-					sprintf(line, "ANALYSIS: V: %d %s <-> %s:%d (%d %s)\n", atom_cnt+1, myligand->base_atom_types[atomtypeid], curr.res_name, curr.res_id, curr.id, curr.name);
-					result += line;
+				if(!myligand->acceptor[atom_cnt] && !myligand->donor[atom_cnt] &&
+				   !curr.acceptor && !curr.donor){
+					if(dist2 <= 4.2*4.2){
+						sprintf(line, "ANALYSIS: V: %d %s <-> %s:%d (%d %s)\n", atom_cnt+1, myligand->base_atom_types[atomtypeid], curr.res_name, curr.res_id, curr.id, curr.name);
+						result += line;
+					}
 				}
 			}
 		}
@@ -2398,9 +2401,12 @@ float calc_intraE_f(
 										result += line;
 									}
 								} else{
-									if(dist <= 4.2){
-										sprintf(line, "ANALYSIS: V: %d %s <-> %s:%d (%d %s)\n", atom_cnt+1, myligand->base_atom_types[atomtypeid], curr.res_name, curr.res_id, curr.id, curr.name);
-										result += line;
+									if(!myligand->acceptor[atom_cnt] && !myligand->donor[atom_cnt] &&
+									   !curr.acceptor && !curr.donor){
+										if(dist <= 4.2){
+											sprintf(line, "ANALYSIS: V: %d %s <-> %s:%d (%d %s)\n", atom_cnt+1, myligand->base_atom_types[atomtypeid], curr.res_name, curr.res_id, curr.id, curr.name);
+											result += line;
+										}
 									}
 								}
 							}
