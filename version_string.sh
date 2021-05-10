3d3109f3123e368a6440b810e489d73a885fd883
atillack <atillack@scripps.edu>
2021-05-10 18:48:34 -0400

    Take debug text out.

--- version_string.sh
+++ version_string.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version=`git asd describe --abbrev=40 --dirty --always --tags 2>/dev/null`
+version=`git describe --abbrev=40 --dirty --always --tags 2>/dev/null`
 if [[ "${version}" == "" ]]; then
 	version=`cat BASE_VERSION`"-release"
 fi
