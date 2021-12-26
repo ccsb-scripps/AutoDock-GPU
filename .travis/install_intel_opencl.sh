a322347fd462029ce1942f19f65f15f738da0a9d
Leonardo Solis V <leonardosolis86@gmail.com>
2021-12-26 23:54:48 +0100

    Installing package with sudo.

--- .travis/install_intel_opencl.sh
+++ .travis/install_intel_opencl.sh
@@ -18,5 +18,5 @@ PACKAGE_NAME=opencl_runtime_16.1.2_x64_rh_6.4.0.37
 wget -q ${PACKAGE_URL} -O /tmp/opencl_runtime.tgz
 tar -xzf /tmp/opencl_runtime.tgz -C /tmp
 sed 's/decline/accept/g' -i /tmp/${PACKAGE_NAME}/silent.cfg
-apt-get install -yq cpio
+sudo apt-get install -yq cpio
 /tmp/${PACKAGE_NAME}/install.sh -s /tmp/${PACKAGE_NAME}/silent.cfg
