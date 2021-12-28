93fb6cf45bd0e73663ff4c3ea4279a0c6b3332ba
Leonardo Solis V <leonardosolis86@gmail.com>
2021-12-28 16:57:10 +0100

    Removing <sudo> based on GitHub actions (console logs).

--- .travis/install_intel_opencl.sh
+++ .travis/install_intel_opencl.sh
@@ -20,5 +20,5 @@ PACKAGE_NAME=l_opencl_p_18.1.0.015
 wget -q ${PACKAGE_URL} -O /tmp/opencl_runtime.tgz
 tar -xzf /tmp/opencl_runtime.tgz -C /tmp
 sed 's/decline/accept/g' -i /tmp/${PACKAGE_NAME}/silent.cfg
-sudo apt-get install -yq cpio
+apt-get install -yq cpio
 sudo /tmp/${PACKAGE_NAME}/install.sh -s /tmp/${PACKAGE_NAME}/silent.cfg
