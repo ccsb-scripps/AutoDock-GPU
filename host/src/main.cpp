4e889b150b11ed4c16bbbf566d4ad9352a2202c6
Edward Mascarenhas <edward.mascarenhas@intel.com>
2022-05-04 09:21:20 -0700

    Fix include of unnecessary sycl.hpp and dpct include files that break openCL build.

--- host/src/main.cpp
+++ host/src/main.cpp
@@ -22,8 +22,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 */
 
-#include <CL/sycl.hpp>
-#include <dpct/dpct.hpp>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
